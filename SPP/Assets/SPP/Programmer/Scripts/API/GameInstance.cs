b823780851c9bc9a65f6ca5f08b416476e7438a1
DESKTOP-86R37I9\rikuk <bg.hachi98@gmail.com>
2016-10-18 23:23:57 +0900

    SceneManagerの必要なパーツ揃え

--- SPP/Assets/SPP/Programmer/Scripts/API/GameInstance.cs
+++ SPP/Assets/SPP/Programmer/Scripts/API/GameInstance.cs
@@ -15,13 +15,36 @@ using UnityEngine.SceneManagement;
 */
 public class GameInstance : BaseObjectSingleton<GameInstance> {
 
+    // シーンマネージャー
+    [SerializeField]
+    SceneManager m_sceneManagerPrefabs;
+    SceneManager m_sceneManager;
+    public SceneManager mSceneManager
+    {
+        get { return m_sceneManager; }
+        private set { m_sceneManager = value; }
+    }
+
+
+    // BaseObjectのアップデーター
     [SerializeField]
     BaseObjectUpdater m_updaterPrefbs;
     BaseObjectUpdater m_updater;
 
+    // スタティックキャンバス
+    [SerializeField]
+    GameObject m_staticCanvasPrefabs;
+    GameObject m_staticCanvas;
+    public GameObject mStaticCanvas
+    {
+        get { return m_staticCanvas; }
+        private set { m_staticCanvas = value; }
+    }
+
+
 
     /****************************************************************************** 
-    @brief      初期化用。タイミングはAwakeと一緒
+    @brief      初期化用。タイミングはAwakeと一緒。BaseObjectの実装
     @return     none
     */
     protected override void mOnRegistered()
@@ -29,13 +52,26 @@ public class GameInstance : BaseObjectSingleton<GameInstance> {
         base.mOnRegistered();
         mUnregisterList(this); // mUpdateRunを呼び出す必要がないので管理から外す
 
+        // ゲームに必要なプレハブとかを作成する。
+        
+        if(mSceneManager == null)
+        {
+            mSceneManager = mCreate(m_sceneManagerPrefabs) as SceneManager;
+        }
+        mSceneManager.transform.SetParent(this.transform, false);
+
         //初期化するべきオブジェクトの初期化や生成など
         if (m_updater == null)
         {
             m_updater = mCreate(m_updaterPrefbs) as BaseObjectUpdater;
         }
+        m_updater.transform.SetParent(this.transform, false);
 
-        this.transform.SetParent(m_updater.transform, false);
+        if(mStaticCanvas == null)
+        {
+            mStaticCanvas = mCreate(m_staticCanvasPrefabs) as GameObject;
+            mStaticCanvas.name = "StaticCanvas";
+        }
     }
 
 
