d2e49e31dba682da613d19b7aaad651e8d80fd3d
kaneko <iwgm14cyancle@gmail.com>
2016-10-25 23:26:55 +0900

    再コミット

---
+++ SPP/Assets/SPP/Programmer/Scripts/Ship/WindObject.cs
@@ -0,0 +1,46 @@
+﻿/**************************************************************************************/
+/*! @file   WindObject.cs
+***************************************************************************************
+@brief      風とするオブジェクトに設定するクラス
+***************************************************************************************
+@author     Kaneko Kazuki
+***************************************************************************************/
+
+using UnityEngine;
+using System.Collections;
+
+public class WindObject : BaseObject {
+
+    [SerializeField]
+    private float m_windForce;
+
+    [SerializeField]
+    [Range(-180, 180)]
+    private float m_windDirection;
+
+    public float mWindForce
+    {
+        get { return m_windForce; }
+        set { m_windForce = value; }
+    }
+    public float mWindDirection
+    {
+        get { return m_windDirection; }
+        set {
+            //180°以上にはならないようにする
+            if (Mathf.Abs(value) > 180)
+            {
+                m_windDirection = Mathf.Clamp(value, -180, 180);
+            }
+        }
+    }
+
+    // Use this for initialization
+    override protected void Start(){
+    }
+	
+	//// Update is called once per frame
+	//override public void mOnUpdate () {
+	//}
+
+}
