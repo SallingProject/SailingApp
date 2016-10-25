5816214c85fa5ec401055c7ec3e5112e165278b4
DESKTOP-86R37I9\rikuk <bg.hachi98@gmail.com>
2016-10-25 22:20:51 +0900

    入れ直し

---
+++ SPP/Assets/SPP/Programmer/Scripts/API/BaseObjects/BaseObjectSingleton.cs
@@ -0,0 +1,41 @@
+﻿/**************************************************************************************/
+/*! @file   BaseObjectSingleton.cs
+***************************************************************************************
+@brief      シングルトン作成用基底クラス
+***************************************************************************************
+@author     Ko Hashimoto
+***************************************************************************************
+* Copyright © 2016 Ko Hashimoto All Rights Reserved.
+***************************************************************************************/
+
+/******************************************************************************* 
+@brief   シングルトン作成用基底クラス
+*/
+public class BaseObjectSingleton<T> : BaseObject where T : BaseObjectSingleton<T>
+{
+    private static T m_instance;
+    public static T mInstance
+    {
+        get{ return m_instance; }
+    }
+
+    /****************************************************************************** 
+    @brief      インスタンスがあるかの確認用    
+    @return     インスタンスがある：true / ない：false
+    */
+    protected bool mCheckInstance()
+    {
+        if (m_instance == null)
+        {
+            m_instance = (T)this;
+            return true;
+        }
+        else if (m_instance == this)
+        {
+            return true;
+        }
+
+        Destroy(this);
+        return false;
+    }
+}
---
+++ SPP/Assets/SPP/Programmer/Scripts/SceneTitle/SceneTitle.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SceneTitle : BaseObject
+{
+    [SerializeField]
+    Button m_button;
+
+    protected override void mOnRegistered() { }
+
+    public void NextScene()
+    {
+
+    }
+}
