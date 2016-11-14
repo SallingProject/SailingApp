26cc8255e4dcb13952984b03d6ffaac4a37c540e
DESKTOP-86R37I9\rikuk <bg.hachi98@gmail.com>
2016-11-14 13:36:33 +0900

    とりあえず仮状態

---
+++ SPP/Assets/SPP/Programmer/Scripts/SceneLibrary/SceneLibrary.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class SceneLibrary : BaseObject {
+
+    [SerializeField]
+    Image m_touchPanel;
+
+    [SerializeField]
+    GameObject m_shipRoot;
+
+    protected override void mOnRegistered()
+    {
+        mUnregisterList(this);
+
+
+        EventTrigger trigger = m_touchPanel.GetComponent<EventTrigger>();
+
+        // Dragイベントの追加
+        {
+            EventTrigger.Entry drag = new EventTrigger.Entry();
+            drag.eventID = EventTriggerType.Drag;
+            drag.callback.AddListener(eventData =>
+            {
+                var rotation = InputManager.mInstance.GetdeltaPosition(1);
+                m_shipRoot.transform.Rotate(new Vector3(m_shipRoot.transform.rotation.y + rotation[0].Y, m_shipRoot.transform.rotation.x + rotation[0].X, 0));
+            });
+
+            trigger.triggers.Add(drag);
+        }
+    }
+}
