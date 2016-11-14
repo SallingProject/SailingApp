e7701b929c51fb26fad4f925e65724f52fd91f8f
DESKTOP-86R37I9\rikuk <bg.hachi98@gmail.com>
2016-11-14 17:37:58 +0900

    SceneBaseを継承させた

--- SPP/Assets/SPP/Programmer/Scripts/SceneTitle/SceneTitle.cs
+++ SPP/Assets/SPP/Programmer/Scripts/SceneTitle/SceneTitle.cs
@@ -2,12 +2,15 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class SceneTitle : BaseObject
+public class SceneTitle : SceneBase
 {
     [SerializeField]
     Button m_button;
 
-    protected override void mOnRegistered() { }
+    protected override void mOnRegistered()
+    {
+        base.mOnRegistered();
+    }
 
     public void NextScene()
     {
