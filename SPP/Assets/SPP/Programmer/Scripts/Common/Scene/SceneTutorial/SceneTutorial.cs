32422892e8f3b19b3cddd1e8533d487c5d37ffac
youtsuumochii <sugisugirr@gmail.com>
2016-11-21 17:02:13 +0900

    creat Tutreal

---
+++ SPP/Assets/SPP/Programmer/Scripts/Common/Scene/SceneTutorial/SceneTutorial.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SceneTutorial : SceneBase { 
+
+    [SerializeField]
+    Button m_button;
+
+    protected override void mOnRegistered()
+    {
+        base.mOnRegistered();
+    }
+}
