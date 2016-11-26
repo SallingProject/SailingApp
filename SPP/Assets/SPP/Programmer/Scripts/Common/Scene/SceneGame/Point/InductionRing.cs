3dc3f9719763760d2c8dd600824801392fe91543
DESKTOP-86R37I9\rikuk <bg.hachi98@gmail.com>
2016-11-26 22:00:35 +0900

    まっすぐのやつにも対応

--- SPP/Assets/SPP/Programmer/Scripts/Common/Scene/SceneGame/Point/InductionRing.cs
+++ SPP/Assets/SPP/Programmer/Scripts/Common/Scene/SceneGame/Point/InductionRing.cs
@@ -3,20 +3,17 @@ using System.Collections.Generic;
 
 public class InductionRing : BaseObject {
 
-    static readonly int m_kCreateCount = 8;
+    static readonly int m_kCreateCount = 9;
 
     [SerializeField]
     GameObject m_ringPref;
 
     [SerializeField]
-    float m_radius = 0.5f;
+    float m_radius = 10f;
     List<GameObject> m_ringCashList = new List<GameObject>();
 
     [SerializeField]
     Point m_nextpoint;
-
-    [SerializeField]
-    float m_reset;
 
     protected override void mOnRegistered()
     {
@@ -38,25 +35,48 @@ public class InductionRing : BaseObject {
     public void mSetNextPoint(Point nextPoint)
     {
         m_nextpoint = nextPoint;
-        //オブジェクト間の角度差
-        float angleDiff = 180f / (float)m_ringCashList.Count;
 
-        //各オブジェクトを円状に配置
-        for (int i = 0; i < m_ringCashList.Count; i++)
+        if (nextPoint.m_buoyType == Point.eBuoyType.Curve)
         {
-            Vector3 childPostion = transform.position;
+            //オブジェクト間の角度差
+            float angleDiff = 270f / (float)m_ringCashList.Count;
+            //各オブジェクトを円状に配置
+            for (int i = 0; i < m_ringCashList.Count; i++)
+            {
+                Vector3 childPostion = transform.position;
 
-            float angle = (((m_reset + angleDiff) * i) - angleDiff) * Mathf.Deg2Rad + m_nextpoint.transform.eulerAngles.y - 180;
-            childPostion.x += m_radius * Mathf.Cos(angle) + m_nextpoint.transform.position.x;
-            childPostion.z += m_radius * Mathf.Sin(angle) + m_nextpoint.transform.position.z;
+                float angle = ((angleDiff * i) - angleDiff) * Mathf.Deg2Rad + m_nextpoint.transform.eulerAngles.y;
+                childPostion.x += m_radius * Mathf.Cos(angle) + m_nextpoint.transform.position.x;
+                childPostion.z += m_radius * Mathf.Sin(angle) + m_nextpoint.transform.position.z;
 
-            m_ringCashList[i].transform.position = childPostion;
+                m_ringCashList[i].transform.position = childPostion;
+            }
+        }
+        else
+        {
+            int harf = m_ringCashList.Count / 2;
+            //各オブジェクトを円状に配置
+            for (int i = 0; i < m_ringCashList.Count; i++)
+            {
+                Vector3 pointPosition = m_nextpoint.transform.position;
+                if(harf > i)
+                {
+                    m_ringCashList[i].transform.position = new Vector3(pointPosition.x, pointPosition.y, pointPosition.z - (m_radius * i));
+                }
+                else
+                {
+                    m_ringCashList[i].transform.position = new Vector3(pointPosition.x, pointPosition.y, pointPosition.z + (m_radius * (i - harf)));
+                }
+            }
         }
     }
 
+    #region テスト用
+#if true
     void Update()
     {
         mSetNextPoint(m_nextpoint);
     }
-
+#endif
+    #endregion
 }
